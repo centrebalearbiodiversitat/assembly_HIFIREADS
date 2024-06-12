459d93ba75539c44388487ab42d63b7f8c1839fc
Laura Triginer <triginerlaura@gmail.com>
2024-06-12 17:16:11 +0200

    Update scaffolding.sh

--- scaffolding.sh
+++ scaffolding.sh
@@ -1,27 +1,37 @@
-#!/bin/bash
+!/bin/bash
 
+purged=$1
+HIC1=$2
+HIC2=$3
+THREADS=$4
 mkdir bwa_output
 cd bwa_output
 
-bwa index purged.fa
+samtools faidx ${purged}
 ## checking quality with fastqc for HI-C reads
-fastqc ../reads/Tscabra_allHiC_*
+fastqc ${HIC1} ${HIC2}
 # use BWA-MEM to align the Hi-C paired-end reads to reference sequences
 # Step1. HiC reads from FASTQC to BAM
-bwa mem -t 36 purged.fa ../reads/Tscabra_allHiC_1.fastq.gz | samtools view -@ 36 -Sb - > ../reads/Tscabra_allHiC_1.bam
+HIC1_basename=$(basename "${HIC1%.*}")
+HIC2_basename=$(basename "${HIC2%.*}")
+HIC1_output="${HIC1_basename}.filtered.bam"
+HIC2_output="${HIC2_basename}.filtered.bam"
 
-bwa mem -t 36 purged.fa ../reads/Tscabra_allHiC_2.fastq.gz | samtools view -@ 36 -Sb - > ../reads/Tscabra_allHiC_2.bam
+bwa mem -t ${THREADS} ${purged} ${HIC1} | samtools view -@ ${THREADS} -Sb - > "${HIC1_basename}.bam"
+bwa mem -t ${THREADS} ${purged} ${HIC2} | samtools view -@ ${THREADS} -Sb - > "${HIC2_basename}.bam"
 
 #Step2. Retain only the portion of the chimeric read that maps in the 5'-orientation in relation to its read orientation.
 
-samtools view -h ../reads/Tscabra_allHiC_1.bam | perl /opt/filter_five_end.pl | samtools view -Sb > Tscabra_allHiC_1.filtered.bam
-samtools view -h ../reads/Tscabra_allHiC_2.bam | perl /opt/filter_five_end.pl | samtools view -Sb > Tscabra_allHiC_2.filtered.bam
+samtools view -h "${HIC1_basename}.bam" | perl /opt/scripts/filter_five_end.pl | samtools view -Sb > "${HIC1_output}"
+samtools view -h "${HIC2_basename}.bam" | perl /opt/scripts/filter_five_end.pl | samtools view -Sb > "${HIC2_output}"
 
 # Now we pair the filtered single-end Hi-C using "two_read_bam_combiner.pl"
-mkdir temp_dir
+
 REF='/home/bioinfo/tethysbaena_scabra_assembly/bwa_output/purged.fa'
 FAIDX='$REF.fai'
-perl /opt/two_read_bam_combiner.pl Tscabra_allHiC_1.filtered.bam Tscabra_allHiC_2.filtered.bam samtools 10 | samtools view -bS -t $FAIDX | samtools sort -@ 32 -o temp_dir/>
+perl /opt/scripts/two_read_bam_combiner.pl "${HIC1_output}" "${HIC2_output}"  samtools 10 | samtools view -bS -t $FAIDX | samtools sort -@ ${THREADS} -o HiC1_HiC2_combined.bam
 
-yahs purged.hic.asm Tscabra_HiC.bam
+yahs purged.hic.asm HiC1_HiC2_combined.bam
+
+
 
