f39d7bf69b23155872f257caaedcb5caa2e37776
Laura Triginer <triginerlaura@gmail.com>
2024-06-26 14:40:24 +0200

    Update full_pipeline.sh

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -134,3 +134,20 @@ quast.py purged.fa --large --est-ref-size ${genome_size} -o quality_stats/quast
 /opt/gfastats/build/bin/gfastats purged.fa > ${ASM_NAME}_purged.gfastats
 
 echo "Step 2 -- Done. Assembly, purge duplicates and quality assessment prior and after the following steps have been performed."
+
+echo "Step 3 -- Decontamination before scaffolding"
+
+cd ${MAIN_DIR}
+mkdir -p decontamination/fcs_output
+mkdir -p decontamination/whokaryote
+
+## obtain tax_id 
+
+python3 /opt/fcs/fcs.py screen genome --fasta purgedups/purged.fa --out-dir ./fcs_output/ --gx-db /opt/fcs/gxdb/ --tax-id ${tax_id}
+# Delete contaminants:
+cat purged.fa | python3 /opt/fcs/fcs.py clean genome --action-report ./fcs_output/purged.${tax_id}.fcs_gx_report.txt --output ${ASM_NAME}_FCS_clean.fasta --contam-fasta-out ${ASM_NAME}_FCS_contam.fasta
+
+# Whokaryote
+whokaryote.py --contigs clean.fasta --outdir whokaryote_output --f --minsize 10000 --model T
+
+echo "Step 3 -- Done. Decontamination prior scaffolding has been performed"
