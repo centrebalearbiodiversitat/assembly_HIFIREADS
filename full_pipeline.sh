1376396f0b9e55613b0bbafb210b05b28f598b6c
Laura Triginer <triginerlaura@gmail.com>
2024-06-28 11:59:09 +0200

    Update full_pipeline.sh

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -155,8 +155,8 @@ echo "Step 3 -- Done. Decontamination prior scaffolding has been performed"
 
 echo "Step 4 -- Scaffolding using YAHS"
 
-mkdir bwa_output
-cd bwa_output
+mkdir YAHS
+cd YAHS
 
 cp ../decontamination/whokaryote_output/eukaryotes.fa .
 file=eukaryotes.fa
@@ -192,35 +192,38 @@ conda activate busco
 busco -i  ${YAHS_OUTPUT} -c ${THREADS} --lineage ${lineage} -o busco_out -m geno 
 conda deactivate
 /opt/gfastats/build/bin/gfastats -i ${YAHS_OUTPUT} > ${YAHS_OUTPUT}.gfastats
+cd ..
 
 echo "Step 4 -- DONE"
 
 echo "Step 5 -- Preparing Blobtoolkit"
-                                                                                                         
+mkdir -p BlobTool
+cd BlobTool
+cp ../YAHS/${YAHS_OUTPUT} .
 # First,   we need a  alignment.bam, a blast_out and the taxdump database!
 # Alignment:
 #build index of the genome
-bwa index $YAHS_OUTPUT
-bwa mem -5SPM -T30 -t24 $YAHS_OUTPUT ${HIC_1} ${HIC_2} | samtools view -Shb -@ 6 > alignment.bam
+bwa index ${YAHS_OUTPUT}
+bwa mem -5SPM -T30 -t24 ${YAHS_OUTPUT} ../${HIC_1} ../${HIC_2} | samtools view -Shb -@ 6 > alignment.bam
 samtools sort -@ 12 -o alignment_sorted.bam alignment.bam
 samtools flagstat alignment_sorted.bam > alignment_sorted.bam.flagstats
 samtools index alignment_sorted.bam
 # blastn:
-blastn -db nt -query $outfile_fasta -outfmt "6 qseqid staxids bitscore std" -max_target_seqs 1 -max_hsps 1 -evalue 1e-25 -num_threads 64 -out blast.out
+blastn -db nt -query ${YAHS_OUTPUT} -outfmt "6 qseqid staxids bitscore std" -max_target_seqs 1 -max_hsps 1 -evalue 1e-25 -num_threads 64 -out ${YAHS_OUTPUT}_blast.out
 # Tambien  se necesita descargar la base de datos taxdump:
 #wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 #tar -xzvf taxdump.tar.gz
 # RUN BLOBTOOLKIT:
 # Step 1. Create blobtools database:
-blobtools create --fasta $YAHS_OUTPUT  --meta meta.yaml --taxid $tax_id --taxdump . ${ASM_NAME}
+blobtools create --fasta ${YAHS_OUTPUT}  --meta meta.yaml --taxid $tax_id --taxdump . ${ASM_NAME}
 # Step 2. Add blast hits
-blobtools add  --hits blast.out --taxrule bestsumorder --taxdump . ${ASM_NAME}
+blobtools add  --hits ${YAHS_OUTPUT}_blast.out --taxrule bestsumorder --taxdump . ${ASM_NAME}
 # Step 3. Add coverage
 # samtools index -c alignment_sorted.bam ## looks for a .bam.csi
 blobtools add --cov alignment_sorted.bam ${ASM_NAME}
 # Step 4. Add busco scores
-blobtools add --busco busco_out/run_*/full_table.tsv ${ASM_NAME}
+blobtools add --busco ../YAHS/busco_out/run_*/full_table.tsv ${ASM_NAME}
 # Step 5. Open dataset in Blobtoolkit viewer
-blobtools host `pwd`
+blobtools host ${ASM_NAME}
 
 
