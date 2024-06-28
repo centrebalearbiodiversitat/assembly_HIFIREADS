7f8ff7089a05a30772195e75259e60fa4b34f506
Laura Triginer <triginerlaura@gmail.com>
2024-06-28 11:51:38 +0200

    Update full_pipeline.sh

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -158,10 +158,12 @@ echo "Step 4 -- Scaffolding using YAHS"
 mkdir bwa_output
 cd bwa_output
 
-file=$(cat decontamination/whokaryote_ouput/eukaryotes.fa)
+cp ../decontamination/whokaryote_output/eukaryotes.fa .
+file=eukaryotes.fa
+bwa index ${file}
 samtools faidx $file
 ## checking quality with fastqc for HI-C reads
-fastqc ${HIC_1} ${HIC_2}
+fastqc ../${HIC_1} ../${HIC_2}
 # use BWA-MEM to align the Hi-C paired-end reads to reference sequences
 # Step1. HiC reads from FASTQC to BAM
 HIC1_basename=$(basename "${HIC_1%.*}")
@@ -169,8 +171,8 @@ HIC2_basename=$(basename "${HIC_2%.*}")
 HIC1_output="${HIC1_basename}.filtered.bam"
 HIC2_output="${HIC2_basename}.filtered.bam"
 
-bwa mem -t ${THREADS} ${file} ${HIC_1} | samtools view -@ ${THREADS} -Sb - > "${HIC1_basename}.bam"
-bwa mem -t ${THREADS} ${file} ${HIC_2} | samtools view -@ ${THREADS} -Sb - > "${HIC2_basename}.bam"
+bwa mem -t ${THREADS} ${file} ../${HIC_1} | samtools view -@ ${THREADS} -Sb - > "${HIC1_basename}.bam"
+bwa mem -t ${THREADS} ${file} ../${HIC_2} | samtools view -@ ${THREADS} -Sb - > "${HIC2_basename}.bam"
 
 #Step2. Retain only the portion of the chimeric read that maps in the 5'-orientation in relation to its read orientation.
 
@@ -179,11 +181,11 @@ samtools view -h "${HIC2_basename}.bam" | perl /opt/scripts/filter_five_end.pl |
 
 # Now we pair the filtered single-end Hi-C using "two_read_bam_combiner.pl"
 
-REF='${MAIN_DIR}/purgedups/purged.fa'
+REF='${file}'
 FAIDX='$REF.fai'
 perl /opt/scripts/two_read_bam_combiner.pl "${HIC1_output}" "${HIC2_output}"  samtools 10 | samtools view -bS -t $FAIDX | samtools sort -@ ${THREADS} -o HiC1_HiC2_combined.bam
 
-YAHS_OUTPUT=$(yahs purged.hic.asm HiC1_HiC2_combined.bam)
+YAHS_OUTPUT=$(yahs eukaryotes.hic.asm HiC1_HiC2_combined.bam)
 conda activate busco
 busco -i  ${YAHS_OUTPUT} -c ${THREADS} --lineage ${lineage} -o busco_out -m geno 
 conda deactivate
