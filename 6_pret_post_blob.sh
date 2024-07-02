aad5df21dcd53feadd30f888303898e24153575e
Laura Triginer <triginerlaura@gmail.com>
2024-07-02 15:16:20 +0200

    Update 6_pret_post_blob.sh

--- 6_pret_post_blob.sh
+++ 6_pret_post_blob.sh
@@ -3,13 +3,22 @@
 FASTA=$1 #Blobtool output
 HIC_1=$2
 HIC_2=$3
-#Create an index from the YAHS output
-bowtie2-build ${FASTA} -o ${FASTA}_bowtie
+#Create an index from the btk output
+bwa index ${FASTA}
+#bowtie2-build ${FASTA} -o ${FASTA}_bowtie
 # Align reads to reference genome
-bowtie2 -x ${FASTA}_bowtie -1 ../${HIC_1} -2 ../${HIC_2} -S ${FASTA}_aligned.sam
+#bowtie2 -x ${FASTA}_bowtie -1 ../${HIC_1} -2 ../${HIC_2} -S ${FASTA}_aligned.sam
+bwa mem -5SPM -T30 -t24 ${FASTA} ../${HIC_1} ../${HIC_2} | samtools view -Shb -@ 6 > ${FASTA}_alignment.bam
+samtools sort -@ 12 -o ${FASTA}_alignment_sorted.bam alignment.bam
+
 # Convert SAM to BAM and sort
-samtools view -bS ${FASTA}_aligned.sam | samtools sort -o ${FASTA}_alignment_sorted.bam -
+#samtools view -bS ${FASTA}_aligned.sam | samtools sort -o ${FASTA}_alignment_sorted.bam -
 samtools flagstat ${FASTA}_alignment_sorted.bam > ${FASTA}_align_sorted.bam.flagstats
+
+bwa index assembly.fasta
+bwa mem -5SPM -T30 -t24 assembly.fasta ../../../../reads/Tscabra_allHiC_1.fastq.gz ../../../../reads/Tscabra_allHiC_2.fastq.gz | samtools view -Shb -@ 6 > alignment.bam
+samtools sort -@ 12 -o alignment_sorted.bam alignment.bam
+samtools index alignment_sorted.bam
 
 # PretextMap needs an alignment .bam.
 # Obtain .pretext:
